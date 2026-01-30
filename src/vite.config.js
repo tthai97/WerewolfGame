f31fa6c0b229b7e0f6945ad22025eb674457704b
tthai97 <63345679+tthai97@users.noreply.github.com>
2026-01-30 04:35:41 +0000

    remove dummy files

--- src/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace '/WerewolfGame/' with '/<repo-name>/' or '/' if using a custom domain or root.
-export default defineConfig({
-  base: '/WerewolfGame/',
-  plugins: [react()],
-})
